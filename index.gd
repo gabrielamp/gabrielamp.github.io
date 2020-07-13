ff9966035c7460fa505547c09750e7cbca2e47b5
Gabriela Moisescu <gmp@Gabrielas-MacBook-Pro.local>
2020-07-12 20:30:42 -0400

    Change content

--- index.gd
+++ index.gd
@@ -1,3 +1,13 @@
-# Gabby Gab Gab
+# Example Title
 
-This is great.
+## Example subtitle
+
+<def-env>
+A group \( G \) is said to be *abelian* if \( xy = yx \) for every \( x, y \in G \).
+</def-env>
+
+<thm-env>
+Let \( G \) be an abelian group.
+Let \( H \leq G \) be a subgroup.
+Then, \( H \) is a normal subgroup of \( G \).
+</thm-env>
