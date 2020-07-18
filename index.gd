df4a62fd88ffe152b70ea7fa5f8ccdc851565d64
Gabriela Moisescu <gmp@Gabrielas-MacBook-Pro.local>
2020-07-17 20:59:52 -0400

    Add basic info

--- index.gd
+++ index.gd
@@ -1,13 +1,31 @@
-# Example Title
+# My Webpage
 
-## Example subtitle
+My name's Gabriela, and I'm a Mathematics and Computer Science student at McGill University in Montréal, Canada.
+
+I enjoy:
+* algebra
+* machine learning
+* cooking
+* reading
+* drawing
+* sleeping 9 hours
+
+
+## Et al.
+* [S. Elaidi](https://shereenelaidi.github.io)
+* [D. Lopez](https://diegolopez.me)
+* [M. Darmon](https://maiadd.github.io)
+* [V. Nguyen](https://opent03.github.io)
+
+S. Elaidi and D. Lopez gave the following definition in their seminal work *On Gabriela Moisescu-Pareja* (2020):
 
 <def-env>
-A group \( G \) is said to be *abelian* if \( xy = yx \) for every \( x, y \in G \).
+A *Gabriela* is a Human with a grandma-factor \( \gamma \) larger than 1.
+We denote this by \(G[\gamma]\).
 </def-env>
 
 <thm-env>
-Let \( G \) be an abelian group.
-Let \( H \leq G \) be a subgroup.
-Then, \( H \) is a normal subgroup of \( G \).
+A \(G[\gamma]\) satisfies the following properties:
+1. \(G[\gamma]\) likes algebra
+2. \(G[\gamma] \) sleeps at \( 0:00 - \gamma  \) at night
 </thm-env>
