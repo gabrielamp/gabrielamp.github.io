d74722f025acbfde6336c7fb4a061891b07fdd88
Gabriela Moisescu <gabriela.moisescu.pareja@gmail.com>
2020-10-21 17:23:23 -0400

    Fix typo

--- index.gd
+++ index.gd
@@ -1,9 +1,9 @@
 Hi there! My name is Gabriela. I'm a final year Mathematics and Computer Science student at McGill University in Montréal, Canada.
 I am currently a research assistant at the [RL Lab](http://rl.cs.mcgill.ca)/[Mila](https://mila.quebec) where I work on abstraction in reinforcement learning.
-My interests include foundations of machine learning, probability theory, absract algebra as well as AI for climate change and social good.
+My interests include foundations of machine learning, probability theory, abstract algebra as well as AI for climate change and social good.
 
 
-## Et al.
+## Cool people
 * [S. Elaidi](https://shereenelaidi.github.io)
 * [D. Lopez](https://diegolopez.me)
 * [M. Darmon](https://maiadd.github.io)
